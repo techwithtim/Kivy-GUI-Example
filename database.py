a6e7871cc4077bcc9fe24825198bfff2ca9cf050
Tech With Tim <35348871+techwithtim@users.noreply.github.com>
2019-02-12 07:21:28 -0500

    Add files via upload

---
+++ database.py
@@ -0,0 +1,51 @@
+import datetime
+
+
+class DataBase:
+    def __init__(self, filename):
+        self.filename = filename
+        self.users = None
+        self.file = None
+        self.load()
+
+    def load(self):
+        self.file = open(self.filename, "r")
+        self.users = {}
+
+        for line in self.file:
+            email, password, name, created = line.strip().split(";")
+            self.users[email] = (password, name, created)
+
+        self.file.close()
+
+    def get_user(self, email):
+        if email in self.users:
+            return self.users[email]
+        else:
+            return -1
+
+    def add_user(self, email, password, name):
+        if email.strip() not in self.users:
+            self.users[email.strip()] = (password.strip(), name.strip(), DataBase.get_date())
+            self.save()
+            return 1
+        else:
+            print("Email exists already")
+            return -1
+
+    def validate(self, email, password):
+        if self.get_user(email) != -1:
+            return self.users[email][0] == password
+        else:
+            return False
+
+    def save(self):
+        with open(self.filename, "w") as f:
+            for user in self.users:
+                f.write(user + ";" + self.users[user][0] + ";" + self.users[user][1] + ";" + self.users[user][2] + "\n")
+
+    @staticmethod
+    def get_date():
+        return str(datetime.datetime.now()).split(" ")[0]
+
+
---
+++ main.py
@@ -0,0 +1,112 @@
+from kivy.app import App
+from kivy.lang import Builder
+from kivy.uix.screenmanager import ScreenManager, Screen
+from kivy.properties import ObjectProperty
+from kivy.uix.popup import Popup
+from kivy.uix.label import Label
+from database import DataBase
+
+
+class CreateAccountWindow(Screen):
+    namee = ObjectProperty(None)
+    email = ObjectProperty(None)
+    password = ObjectProperty(None)
+
+    def submit(self):
+        if self.namee.text != "" and self.email.text != "" and self.email.text.count("@") == 1 and self.email.text.count(".") > 0:
+            if self.password != "":
+                db.add_user(self.email.text, self.password.text, self.namee.text)
+
+                self.reset()
+
+                sm.current = "login"
+            else:
+                invalidForm()
+        else:
+            invalidForm()
+
+    def login(self):
+        self.reset()
+        sm.current = "login"
+
+    def reset(self):
+        self.email.text = ""
+        self.password.text = ""
+        self.namee.text = ""
+
+
+class LoginWindow(Screen):
+    email = ObjectProperty(None)
+    password = ObjectProperty(None)
+
+    def loginBtn(self):
+        if db.validate(self.email.text, self.password.text):
+            MainWindow.current = self.email.text
+            self.reset()
+            sm.current = "main"
+        else:
+            invalidLogin()
+
+    def createBtn(self):
+        self.reset()
+        sm.current = "create"
+
+    def reset(self):
+        self.email.text = ""
+        self.password.text = ""
+
+
+class MainWindow(Screen):
+    n = ObjectProperty(None)
+    created = ObjectProperty(None)
+    email = ObjectProperty(None)
+    current = ""
+
+    def logOut(self):
+        sm.current = "login"
+
+    def on_enter(self, *args):
+        password, name, created = db.get_user(self.current)
+        self.n.text = "Account Name: " + name
+        self.email.text = "Email: " + self.current
+        self.created.text = "Created On: " + created
+
+
+class WindowManager(ScreenManager):
+    pass
+
+
+def invalidLogin():
+    pop = Popup(title='Invalid Login',
+                  content=Label(text='Invalid username or password.'),
+                  size_hint=(None, None), size=(400, 400))
+    pop.open()
+
+
+def invalidForm():
+    pop = Popup(title='Invalid Form',
+                  content=Label(text='Please fill in all inputs with valid information.'),
+                  size_hint=(None, None), size=(400, 400))
+
+    pop.open()
+
+
+kv = Builder.load_file("my.kv")
+
+sm = WindowManager()
+db = DataBase("users.txt")
+
+screens = [LoginWindow(name="login"), CreateAccountWindow(name="create"),MainWindow(name="main")]
+for screen in screens:
+    sm.add_widget(screen)
+
+sm.current = "login"
+
+
+class MyMainApp(App):
+    def build(self):
+        return sm
+
+
+if __name__ == "__main__":
+    MyMainApp().run()
